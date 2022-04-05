5bcd0ac87c21a7b971816ed601167e2f233641c5
tjdwoocn <tjdwoocn@gmail.com>
2022-04-05 17:00:43 +0900

    remove some codes

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -26,65 +26,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// app.post('/register', (req, res) => {
-//     // 회원 가입 할 때 필요한 정보들을 clinet에서 가져오면 
-//     // 그것들을 데이터 베이스에 넣어주기
-//     const user = new User(req.body)
-//     user.save((err, userInfo) => {
-//             if (err) return res.json({ success: false, err, user })
-//             console.log(userInfo.body)
-//             return res.status(200).json({
-//                 success: true
-//             })
-//         }) // mongo db에서 오는 method. // Request에 대해 에러가 나면 json 형태로 에러메세지 저장, 성공하면 success
-// })
-
-// app.post('/register', (req, res) => {
-//     //회원가입 할때 필요한 정보들을 client에서 가져오면 
-//     //그것들을 데이터 베이스에 넣어준다.
-//     const user = new User(req.body)
-//     user.save((err, userInfo) => {
-//         console.log(userInfo);
-//         console.log(user);
-
-
-//         if (err)
-//             return res.json({ success: false, err, user });
-
-//         console.log(userInfo.body);
-
-//         return res.status(200).json({
-
-//             success: true
-
-//         })
-
-//     })
-
-// })
-
-
-
 app.post('/register', (req, res) => {
-    //회원가입 할때 필요한 정보들을 client에서 가져오면 
-    //그것들을 데이터 베이스에 넣어준다.
+    // 회원 가입 할 때 필요한 정보들을 clinet에서 가져오면 
+    // 그것들을 데이터 베이스에 넣어주기
     const user = new User(req.body)
     user.save((err, userInfo) => {
-
-        if (err)
-            return res.json({ success: false, err, user });
-
-
-        return res.status(200).json({
-
-            success: true
-
-        })
-
-    })
-
+            if (err) return res.json({ success: false, err, user })
+            return res.status(200).json({
+                success: true
+            })
+        }) // mongo db에서 오는 method. // Request에 대해 에러가 나면 json 형태로 에러메세지 저장, 성공하면 success
 })
-
 
 
 app.listen(port, () => {
