8ffa75a2c16e33037f63a17848e5783590c36dd3
tjdwoocn <tjdwoocn@gmail.com>
2022-04-08 08:51:37 +0900

    10강내용 추가

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -34,6 +34,10 @@ app.get('/', (req, res) => {
     res.send('Hello World! Today is Wednesday!')
 })
 
+
+
+
+// Register Route
 app.post('/register', (req, res) => {
     // 회원 가입 할 때 필요한 정보들을 clinet에서 가져오면 
     // 그것들을 데이터 베이스에 넣어주기
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +33,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+
+// User model의 유저 정보를 저장하기 전에 무엇인가 하게 함
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    // 다른 정보가 아닌 '비밀번호만' 바꿀때 암호화 작업이 이루어 지도록 조건문 추가
+    if (user.isModified('password')) {
+        // 비밀번호를 암호화 시킨다
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+})
+
 
 const User = mongoose.model('User', userSchema)
 
