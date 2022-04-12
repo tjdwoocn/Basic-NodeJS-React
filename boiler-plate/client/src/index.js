55de923f2cc3cffbe3935e74c740c9059bbfdf35
tjdwoocn <tjdwoocn@gmail.com>
2022-04-12 17:02:38 +0900

    26강 까지의 내용 추가

--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
